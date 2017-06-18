32262cbacfc8ea7ef2deeb16d06e48aedae912b0
BlahGeek <i@BlahGeek.com>
2017-06-18 15:48:56 +0800

    add emoji plugin

---
+++ plugins/emoji.minions/run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: BlahGeek
+# @Date:   2017-06-18
+# @Last Modified by:   BlahGeek
+# @Last Modified time: 2017-06-18
+
+from __future__ import print_function
+from os import path
+import json
+
+
+data = json.load(open(path.join(path.dirname(__file__), 'emojis.json')))
+data = [{
+            'title': props['char'] + ' ' + name,
+            'subtitle': ' '.join(props['keywords']),
+            'badge': props['category'],
+            'data_text': props['char'],
+        } for name, props in data.items() if props.get('char')]
+
+print(json.dumps({'results': data}, indent=4))

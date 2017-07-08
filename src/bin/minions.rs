5b2fc631d27773bca012728f1b40ad3837f1e8a5
BlahGeek <i@BlahGeek.com>
2017-07-08 18:29:23 +0800

    rename example.toml to default.toml, set default config path

--- src/bin/minions.rs
+++ src/bin/minions.rs
@@ -2,7 +2,7 @@
 * @Author: BlahGeek
 * @Date:   2017-06-20
 * @Last Modified by:   BlahGeek
-* @Last Modified time: 2017-07-01
+* @Last Modified time: 2017-07-08
 */
 
 extern crate minions;
@@ -17,6 +17,8 @@ extern crate log;
 #[cfg(feature="use-gtk")]
 extern crate gtk;
 
+use std::env;
+use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -63,21 +65,41 @@ fn main() {
                                       .help("Quicksend content from clipboard"))
                         .get_matches();
 
-    let configfile = args.value_of("config");
+    let default_configcontent : String = include_str!("../../config/default.toml").into();
+    let default_config = default_configcontent.parse::<toml::Value>().unwrap();
+
+    let configfile = match args.value_of("config") {
+        Some(filename) => Path::new(&filename).to_path_buf(),
+        None => env::home_dir().unwrap().join(".minions.toml"),
+    };
     let mut configcontent = String::new();
-    if let Some(configfile) = configfile {
-        info!("Reading config from {}", configfile);
+
+    {
+        info!("Reading config from {:?}", configfile);
         let fin = File::open(&configfile);
         if let Ok(mut fin) = fin {
             let _ = fin.read_to_string(&mut configcontent);
         }
     }
-    if configcontent.len() == 0 {
-        warn!("Using default builtin config");
-        configcontent = include_str!("../../config/example.toml").into();
-    }
 
-    let config = configcontent.parse::<toml::Value>().expect("Invalid config file");
+    let config = if configcontent.len() == 0 {
+        warn!("Using default builtin config");
+        default_config
+    } else {
+        let mut config = configcontent.parse::<toml::Value>().expect("Invalid config file");
+        {
+            let config_map = config.as_table_mut().unwrap();
+            let default_config = default_config.as_table().unwrap();
+            for (key, value) in default_config.into_iter() {
+                if !config_map.contains_key(key) {
+                    warn!("Section {} missing from config, use default", key);
+                    config_map.insert(key.clone(), value.clone());
+                }
+            }
+        }
+        config
+    };
+
     let from_clipboard = args.is_present("from_clipboard");
 
     if args.is_present("rofi") {

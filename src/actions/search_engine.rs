5b5d3b7224e2ec9b9485d35ef2085cc7b89da419
BlahGeek <i@BlahGeek.com>
2017-08-06 13:20:34 +0800

    support suggestion_url for search engines

--- src/actions/search_engine.rs
+++ src/actions/search_engine.rs
@@ -2,23 +2,31 @@
 * @Author: BlahGeek
 * @Date:   2017-06-17
 * @Last Modified by:   BlahGeek
-* @Last Modified time: 2017-07-09
+* @Last Modified time: 2017-08-06
 */
 
 extern crate url;
+extern crate reqwest;
+extern crate serde_json;
 
 use self::url::percent_encoding::{utf8_percent_encode, DEFAULT_ENCODE_SET};
 use toml;
 
-use mcore::action::{Action, ActionResult};
+use std::sync::Arc;
+use std::io::Read;
+
+use mcore::action::{Action, ActionResult, ActionArg};
 use mcore::item::{Item, Icon};
 use actions::utils::open;
 
+#[derive(Clone)]
 pub struct SearchEngine {
     /// Name of the search engine
     name: String,
     /// The URL of the target, replace %s with search text
     address: String,
+    /// The URL for suggestion, open search protocol
+    suggestion_url: Option<String>,
 }
 
 
@@ -32,6 +40,7 @@ impl Action for SearchEngine {
     }
 
     fn accept_text(&self) -> bool { true }
+    fn accept_text_realtime(&self) -> bool { self.suggestion_url.is_some() }
 
     fn run_text(&self, text: &str) -> ActionResult {
         let text = utf8_percent_encode(text, DEFAULT_ENCODE_SET).to_string();
@@ -40,12 +49,44 @@ impl Action for SearchEngine {
         open::that(&url)?;
         Ok(Vec::new())
     }
+
+    fn run_text_realtime(&self, text: &str) -> ActionResult {
+        let url = self.suggestion_url.as_ref().unwrap().replace("%s", text);
+
+        let mut result = String::new();
+        reqwest::get(&url)?.read_to_string(&mut result)?;
+        let result : serde_json::Value = serde_json::from_str(&result)?;
+
+        let suggestions = match result.get(1) {
+            Some(&serde_json::Value::Array(ref arr)) => arr.clone(),
+            _ => Vec::new(),
+        };
+        let suggestions_desc = match result.get(2) {
+            Some(&serde_json::Value::Array(ref arr)) => arr.clone(),
+            _ => vec![serde_json::Value::String(String::new()); suggestions.len()],
+        };
+
+        Ok(
+        suggestions.iter().zip(suggestions_desc.iter())
+            .filter(|&(a, b)| a.is_string() && b.is_string())
+            .map(|(a, b)| {
+                let mut item = Item::new_text_item(a.as_str().unwrap());
+                item.subtitle = Some(b.as_str().unwrap().into());
+                item.action = Some(Arc::new(Box::new(self.clone())));
+                item.action_arg = ActionArg::Text(item.title.clone());
+                item
+            })
+            .collect::<Vec<Item>>()
+        )
+    }
+
 }
 
 #[derive(Deserialize)]
 struct ConfigSite {
     name: String,
     address: String,
+    suggestion_url: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -60,10 +101,11 @@ impl SearchEngine {
             Ok(config) =>
                 config.sites.into_iter()
                 .map(|site| {
-                    debug!("Load search engine: {} = {}", site.name, site.address);
+                    debug!("Load search engine: {} = {} ({:?})", site.name, site.address, site.suggestion_url);
                     SearchEngine {
                         name: site.name,
                         address: site.address,
+                        suggestion_url: site.suggestion_url,
                     }
                 })
                 .collect(),

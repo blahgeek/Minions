f2d43f3c9fabbda0e65c975bd2470e034ab8c60d
BlahGeek <i@BlahGeek.com>
2017-08-13 11:32:43 +0800

    move appimage build script to subfolder, check updates at startup

--- scripts/appimage/build.sh
+++ scripts/appimage/build.sh
@@ -2,9 +2,11 @@
 # @Author: BlahGeek
 # @Date:   2017-07-09
 # @Last Modified by:   BlahGeek
-# @Last Modified time: 2017-08-11
+# @Last Modified time: 2017-08-13
 
 cd "$(dirname "$0")"
+cd ../../
+
 cargo build --release --verbose
 
 VERSION=$(cat Cargo.toml | grep version | head -n 1 | sed -e 's/version = //; s/"//g')
@@ -19,29 +21,17 @@ mkdir -p build/$APP/$APP.AppDir/usr/lib/
 mkdir -p build/$APP/$APP.AppDir/usr/share/
 mkdir -p build/$APP/$APP.AppDir/etc/
 
-cp minions.desktop build/$APP/$APP.AppDir
-cp minions.png build/$APP/$APP.AppDir
+cp scripts/appimage/minions.desktop build/$APP/$APP.AppDir
+cp scripts/appimage/minions.png build/$APP/$APP.AppDir
 cp target/release/minions build/$APP/$APP.AppDir/usr/bin/minions
 cp -r plugins build/$APP/$APP.AppDir/usr/share/minions-plugins
 cp config/fonts.conf build/$APP/$APP.AppDir/etc/fonts.conf
 cp -r fonts build/$APP/$APP.AppDir/fonts
 
-cat > build/$APP/$APP.AppDir/usr/bin/minions.wrapper << EOF
-#!/bin/sh
-cd "\$(dirname "\$0")"
-cd ../../
-
-echo "Building font cache..."
-mkdir -p ~/.minions/fontconfig/
-export FONTCONFIG_FILE="\$(pwd)/etc/fonts.conf"
-fc-cache
-
-unset XDG_DATA_DIRS
-minions "\$@"
-EOF
-
-chmod a+x build/$APP/$APP.AppDir/usr/bin/minions.wrapper
-sed -i "s/Exec=minions/Exec=minions.wrapper/" build/$APP/$APP.AppDir/minions.desktop
+cat scripts/appimage/minions.wrapper.sh | \
+    sed "s/__CURRENT_VERSION__/Minions-$VERSION/g" > \
+        build/$APP/$APP.AppDir/usr/bin/minions.wrapper.sh
+chmod a+x build/$APP/$APP.AppDir/usr/bin/minions.wrapper.sh
 
 cd build/$APP
 wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh -O ./functions.sh
---
+++ scripts/appimage/minions.wrapper.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# @Author: BlahGeek
+# @Date:   2017-08-13
+# @Last Modified by:   BlahGeek
+# @Last Modified time: 2017-08-13
+
+cd "$(dirname "$0")"
+cd ../../
+
+check_update() {
+    RELEASE_URL="https://api.github.com/repos/blahgeek/Minions/releases/latest"
+    LATEST_VERSION=$(curl -q "$RELEASE_URL" | \
+                     grep -o "Minions-.\+-[0-9a-f]\{6\}" | \
+                     head -n 1)
+    if [ "$LATEST_VERSION" != "__CURRENT_VERSION__" ]; then
+        MSG="New version $LATEST_VERSION found. "
+        MSG="${MSG}Current version is __CURRENT_VERSION__. "
+        MSG="${MSG}Visit <a href=\"https://github.com/blahgeek/Minions/releases\">Release page</a> to download."
+        echo "$MSG"
+        zenity --info --text "$MSG"
+    fi
+}
+
+echo "Building font cache..."
+mkdir -p ~/.minions/fontconfig/
+export FONTCONFIG_FILE="$(pwd)/etc/fonts.conf"
+fc-cache
+echo "Build font cache done"
+
+unset XDG_DATA_DIRS
+check_update &
+minions "$@"

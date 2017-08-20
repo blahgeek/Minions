622ac356ea8d13d8bd6655df7ef7dcb7368d2edd
BlahGeek <i@BlahGeek.com>
2017-08-20 20:48:43 +0800

    fix get-public-ip action if network is down

--- plugins/get-public-ip.minions/run.sh
+++ plugins/get-public-ip.minions/run.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash -ex
 # @Author: BlahGeek
 # @Date:   2017-07-15
 # @Last Modified by:   BlahGeek
-# @Last Modified time: 2017-08-09
+# @Last Modified time: 2017-08-20
 
+IP="$(curl http://whatismyip.akamai.com/)"
 echo "
 [{
-    \"title\": \"$(curl http://whatismyip.akamai.com/)\",
+    \"title\": \"$IP\",
     \"icon\": \"character:FontAwesome:\"
 }]
 "
